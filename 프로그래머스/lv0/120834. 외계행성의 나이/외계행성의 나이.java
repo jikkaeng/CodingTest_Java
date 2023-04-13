ccc24785122cd1be803c62ecc8b2b0c067aef65b
JIYEON YU <128116477+jikkaeng@users.noreply.github.com>
2023-04-13 11:38:46 +0900

    [level 0] Title: 외계행성의 나이, Time: 17.47 ms, Memory: 79.4 MB -BaekjoonHub

--- 프로그래머스/lv0/120834. 외계행성의 나이/외계행성의 나이.java
+++ 프로그래머스/lv0/120834. 외계행성의 나이/외계행성의 나이.java
@@ -3,11 +3,11 @@ class Solution {
         String answer = "";
         String s_copy="";
         while(age>0){
-            s_copy+=(char)(age%10+97);
+            answer=(char)(age%10+97)+answer;
             age/=10;
         }
-        StringBuffer sb = new StringBuffer(s_copy);
-        answer = sb.reverse().toString();
+        // StringBuffer sb = new StringBuffer(s_copy);
+        // answer = sb.reverse().toString();
         // int a='a'-97;
         // System.out.println(answer+=(char)(age%10+97));
         return answer;
