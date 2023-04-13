c2513f3bacd1ca6e332141593ac6edd9cc8a38aa
JIYEON YU <128116477+jikkaeng@users.noreply.github.com>
2023-04-13 13:28:45 +0900

    [level 0] Title: 가장 큰 수 찾기, Time: 0.02 ms, Memory: 73.5 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120899. 가장 큰 수 찾기/가장 큰 수 찾기.java
@@ -0,0 +1,13 @@
+class Solution {
+    public int[] solution(int[] array) {
+        int[] answer = new int[2];
+        int max=array[0];
+        for(int i=0;i<array.length;i++){
+            if(array[i]>max){
+                max=array[i];
+                answer[1]=i;
+            }
+        }answer[0]=max;
+        return answer;
+    }
+}
