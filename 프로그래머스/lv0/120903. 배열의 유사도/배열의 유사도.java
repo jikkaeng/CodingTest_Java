29514d112cecdb8deb05719c481fe1e5ed047361
JIYEON YU <128116477+jikkaeng@users.noreply.github.com>
2023-04-17 15:30:31 +0900

    [level 0] Title: 배열의 유사도, Time: 0.02 ms, Memory: 79.7 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120903. 배열의 유사도/배열의 유사도.java
@@ -0,0 +1,12 @@
+import java.util.ArrayList;
+class Solution {
+    public int solution(String[] s1, String[] s2) {
+        int answer = 0;
+        for(String i:s1){
+            for(String j:s2){
+                if(i.equals(j))answer++;
+            }
+        }
+        return answer;
+    }
+}
