f7e3199e71c5421683f0040eb75feae2de428df5
JIYEON YU <128116477+jikkaeng@users.noreply.github.com>
2023-04-24 13:43:46 +0900

    [level 0] Title: 배열 회전시키기, Time: 0.02 ms, Memory: 85.6 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120844. 배열 회전시키기/배열 회전시키기.java
@@ -0,0 +1,14 @@
+class Solution {
+    public int[] solution(int[] numbers, String direction) {
+        int[] answer = new int[numbers.length];
+        if(direction.equals("right")){
+            System.arraycopy(numbers, 0, answer, 1, numbers.length-1);
+            answer[0]=numbers[numbers.length-1];
+        }
+        else{
+            System.arraycopy(numbers, 1, answer, 0, numbers.length-1);
+            answer[answer.length-1]=numbers[0];
+        }
+        return answer;
+    }
+}
