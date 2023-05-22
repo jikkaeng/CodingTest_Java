0713dedb69e2b9a8518f8400253283c31038d5b6
JIYEON YU <128116477+jikkaeng@users.noreply.github.com>
2023-05-22 21:09:22 +0900

    [level 0] Title: 겹치는 선분의 길이, Time: 0.03 ms, Memory: 71.6 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120876. 겹치는 선분의 길이/겹치는 선분의 길이.java
@@ -0,0 +1,34 @@
+class Solution {
+    public int solution(int[][] lines) {
+        int answer = 0;
+	        int min=lines[0][0];
+	        int max=lines[0][1];
+	        for(int i=1;i<3;i++){
+	            if(min>lines[i][0])min=lines[i][0];
+	            if(max<lines[i][1])max=lines[i][1];
+	        }
+	        int cnt=0;
+	        if(max<0)cnt=(-min)+max;
+	        else cnt=max-min;
+	        int[] arr=new int[cnt];
+	        if(min<0) {
+	        	min=-min;
+	        	for(int i=0;i<3;i++){
+		            for(int j=lines[i][0];j<lines[i][1];j++){
+		                arr[j+min]+=1;
+		            }
+		        }
+	        }
+	        else {
+	        	for(int i=0;i<3;i++){
+		            for(int j=lines[i][0];j<lines[i][1];j++){
+		                arr[j-min]+=1;
+		            }
+		        }
+	        }
+	        for(int i:arr){
+	            if(i>=2)answer+=1;
+	        }
+        return answer;
+    }
+}
