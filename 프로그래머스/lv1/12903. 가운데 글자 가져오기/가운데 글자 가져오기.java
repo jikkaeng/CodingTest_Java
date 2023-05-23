174856e9eb7c39ed94c06467ffca7b0f1db17252
JIYEON YU <128116477+jikkaeng@users.noreply.github.com>
2023-05-23 21:31:46 +0900

    [level 1] Title: 가운데 글자 가져오기, Time: 9.51 ms, Memory: 80.1 MB -BaekjoonHub

---
+++ 프로그래머스/lv1/12903. 가운데 글자 가져오기/가운데 글자 가져오기.java
@@ -0,0 +1,15 @@
+class Solution {
+    public String solution(String s) {
+        String answer = "";
+        if(s.length()%2==0)
+        {
+            answer+=s.charAt(s.length()/2-1);
+            answer+=s.charAt(s.length()/2);
+        }
+        else
+        {
+            answer+=s.charAt(s.length()/2);
+        }
+        return answer;
+    }
+}
