5c0909382b6cbc565e81fbe0a387bcee47659267
JIYEON YU <128116477+jikkaeng@users.noreply.github.com>
2023-05-24 13:47:37 +0900

    [level 0] Title: 평행, Time: 0.02 ms, Memory: 75.5 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120875. 평행/평행.java
@@ -0,0 +1,13 @@
+import java.util.ArrayList;
+import java.util.Collections;
+class Solution {
+    public int solution(int[][] dots) {
+        int answer = 0;
+        int cnt=0;
+        if(((double)dots[0][1]-dots[1][1])/((double)dots[0][0]-dots[1][0])==((double)dots[2][1]-dots[3][1])/((double)dots[2][0]-dots[3][0]))answer=1;
+        else if(((double)dots[0][1]-dots[2][1])/((double)dots[0][0]-dots[2][0])==((double)dots[1][1]-dots[3][1])/((double)dots[1][0]-dots[3][0]))answer=1;
+        else if(((double)dots[0][1]-dots[3][1])/((double)dots[0][0]-dots[3][0])==((double)dots[1][1]-dots[2][1])/((double)dots[1][0]-dots[2][0]))answer=1;
+        
+        return answer;
+    }
+}
