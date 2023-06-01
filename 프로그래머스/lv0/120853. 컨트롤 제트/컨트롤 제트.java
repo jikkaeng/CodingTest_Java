1e4796b58336f3d11d8f72c667321078e010c567
JIYEON YU <128116477+jikkaeng@users.noreply.github.com>
2023-06-01 10:27:34 +0900

    [level 0] Title: 컨트롤 제트, Time: 0.12 ms, Memory: 76 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120853. 컨트롤 제트/컨트롤 제트.java
@@ -0,0 +1,13 @@
+class Solution {
+    public int solution(String s) {
+        int answer = 0;
+        String[] arr=s.split(" ");
+        for(int i=0;i<arr.length-1;i++){
+            if(!arr[i].equals("Z")){
+                if(!arr[i+1].equals("Z"))answer+=Integer.parseInt(arr[i]);
+            } 
+        }
+        if(!arr[arr.length-1].equals("Z"))answer+=Integer.parseInt(arr[arr.length-1]);
+        return answer;
+    }
+}
