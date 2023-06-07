7f774cef7088822ae8a0a97e3d4e0696e09ed84a
JIYEON YU <128116477+jikkaeng@users.noreply.github.com>
2023-06-08 02:16:50 +0900

    옹알이 （1）.java

--- 프로그래머스/lv0/120956. 옹알이 （1）/옹알이 （1）.java
+++ 프로그래머스/lv0/120956. 옹알이 （1）/옹알이 （1）.java
@@ -2,11 +2,11 @@ class Solution {
     public int solution(String[] babbling) {
         int answer = 0;
         for(String s:babbling){
-            if(s.contains("aya"))s=s.replaceAll("aya",",");
-            if(s.contains("ye"))s=s.replaceAll("ye",",");
-            if(s.contains("woo"))s=s.replaceAll("woo",",");
-            if(s.contains("ma"))s=s.replaceAll("ma",",");
-            s=s.replaceAll(",","");
+            if(s.contains("aya"))s=s.replaceAll("aya"," ");
+            if(s.contains("ye"))s=s.replaceAll("ye"," ");
+            if(s.contains("woo"))s=s.replaceAll("woo"," ");
+            if(s.contains("ma"))s=s.replaceAll("ma"," ");
+            s=s.replaceAll(" ","");
             if(s.equals(""))answer+=1;
         }
         return answer;
