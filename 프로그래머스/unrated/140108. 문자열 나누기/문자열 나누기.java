2ba240d79f5e5535576bdacb22c9acd4ad22aa53
JIYEON YU <128116477+jikkaeng@users.noreply.github.com>
2023-06-26 13:59:44 +0900

    [unrated] Title: 문자열 나누기, Time: 7.45 ms, Memory: 83.5 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/140108. 문자열 나누기/문자열 나누기.java
@@ -0,0 +1,22 @@
+class Solution {
+    public int solution(String s) {
+        int answer = 1;
+        int pre=0;
+        int next=0;
+        String[] arr=s.split("");
+        String x=arr[0];
+        for(int i=0;i<arr.length;i++){
+            if(i==arr.length-1)break;
+            if(x.equals(arr[i]))pre++;
+            else next++;
+            if(pre==next){
+                answer++;
+                if(i+1<arr.length)x=arr[i+1];
+                pre=0;
+                next=0;
+            }
+        }
+        //if(answer==0)answer=1;
+        return answer;
+    }
+}
